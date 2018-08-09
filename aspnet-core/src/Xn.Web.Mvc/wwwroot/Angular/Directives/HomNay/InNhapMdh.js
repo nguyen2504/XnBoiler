c04c54a6f70c99f27d01d05bca865a5524ed4212
nguyen2504 <34934752+nguyen2504@users.noreply.github.com>
2018-08-09 14:32:41 +0700

    2

---
+++ aspnet-core/src/Xn.Web.Mvc/wwwroot/Angular/Directives/HomNay/InNhapMdh.js
@@ -0,0 +1,38 @@
+﻿
+app.directive('printInputCode', function() {
+  return {
+    restrict: 'AE',
+    templateUrl: '/Angular/Directives/HomNay/InMdhNhap.html',
+    scope: {
+      infor: '=',
+      maunhap: '=',
+      nhaps: '='
+    },
+    link: function (scope, element, $http) {
+      scope.$watch("scope.nhaps", function () {
+        console.log("ok");
+      });
+
+    }
+  };
+});
+
+function fn($scope) {
+    return {
+      restrict:'AE',
+      templateUrl:'/Angular/Directives/HomNay/InMdhNhap.html',
+     scope: {
+       infor: '=',
+       maunhap: '=',
+       nhaps: '='
+      },
+      link: function (scope, element) {
+    
+        $scope.$watch('nhaps', function () {
+          console.log('aaaaaaaaaaaaaaaaa');
+          console.log(JSON.stringify(scope.nhaps));
+        });
+      
+      }
+    };
+  };
